526fa170eef4afea986a84fc9ce7c8700189c272
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-24 00:18:50 -0300

    Create app.js

---
+++ App/View/src/components/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './login'
+import styled from 'styled-components'
+import Div from '../styles/index'
+
+export default function App(){
+    return (
+        <Div>
+        <header> </header>
+            <main>
+                <Login></Login>
+            </main>
+        <footer></footer>
+        </Div>
+    )
+}
